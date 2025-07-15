edf948fd834c9ccca6c41ea55f6a0079115a4f37
BenioshJk <robert09ui@gmail.com>
2025-07-15 04:34:25 -0500

    Actualizacion Pagina Web

---
+++ AVIONES/Login.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFFr82EsjREFaIHcfIxaB6wQyd2TCeTD8",
+    authDomain: "vehiculos-882ec.firebaseapp.com",
+    projectId: "vehiculos-882ec",
+    storageBucket: "vehiculos-882ec.firebasestorage.app",
+    messagingSenderId: "137656962309",
+    appId: "1:137656962309:web:8980add3ee5617ebc67b6c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
